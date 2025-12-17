62fae38132f84107f978574d962e5757cccb75a4
Nelson Ahli <ahlinelson04@gmail.com>
2025-12-17 15:37:43 +0700

    actions test 12

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -72,7 +72,7 @@ if __name__ == "__main__":
         
         mlflow.sklearn.log_model(
             sk_model=best_rf_rs,
-            name="modelRF",
+            artifact_path="modelRF",
             input_example=input_example
         )
         
